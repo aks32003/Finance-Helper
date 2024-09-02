ef74a1129ff341676a042bf9757a4a71f207187c
Akash Singh <102245408+aks32003@users.noreply.github.com>
2024-09-02 14:48:02 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,17 @@ import cors from "cors";
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+const allowedOrigins = ['https://financehelper.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use(express.json());
 
