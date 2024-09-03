75e0739056a3abb88afe09dc8c8d8ba6dd89dd62
Akash Singh <102245408+aks32003@users.noreply.github.com>
2024-09-03 19:47:36 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,29 @@ import cors from "cors";
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
+// CORS whitelist
+const whitelist = [
+  'https://financehelper.vercel.app',
+  'https://another-allowed-origin.com',
+  // add other allowed origins here
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      // Allow request if origin is in the whitelist or if there's no origin (like for server-to-server requests)
+      callback(null, true);
+    } else {
+      // Reject request if origin is not in the whitelist
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 
 const mongoURI: string =
   "mongodb+srv://akashsingh32003:xdVY26A1p7996SrS@financetracker.s4rzira.mongodb.net/";
@@ -20,7 +41,7 @@ mongoose
 app.use("/financial-record", financialRecordRouter);
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Server Running on Port ${port}`);
